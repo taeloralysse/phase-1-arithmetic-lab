8c1636bd49c5eaa8b7f1f423169074ef29217e18
Taelor Lambert <taeloralysse@gmail.com>
2023-03-16 13:49:22 -0400

    math

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 
 // Write your code here
+const num1 = 31;
+const num2 = 2;
+const multiply = num1 * num2
+
+const random = Math.ceil(Math.random() * 10 +2)
+
+let num3 = 4;
+let num4 = 100;
+const mod = num3 % num4
+
+const max = Math.max(16, 17, 18, 19, 20)
